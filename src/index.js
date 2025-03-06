11803f60b92f40223fb23708d33474992af60af5
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-03-06 15:47:53 +0200

    Add bootstrap and purgecss and mini cssextract

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './style.scss';
+
+import * as bootstrap from 'bootstrap';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 import path from 'path';
+import * as glob from 'glob';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { PurgeCSSPlugin } from 'purgecss-webpack-plugin';
 
 export default {
     entry: './src/index.js',
@@ -19,17 +22,33 @@ export default {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.scss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                quietDeps: true,
+                            },
+                        },
+                    }
+                ],
+
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin(),
+        new PurgeCSSPlugin({
+            paths: glob.sync(`./src/**/*`, { nodir: true }),
+        }),
     ],
 };
